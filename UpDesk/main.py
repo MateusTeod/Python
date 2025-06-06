d39e97fb920af8064ed5adde99c5f0bf7a83cc16
mateus <mateus.-teodoro22@outlook.com>
2025-06-06 20:18:24 -0300

    estudos de hoje, foram criados a pagina de abrir chamado

--- UpDesk/main.py
+++ UpDesk/main.py
@@ -1,11 +1,17 @@
 
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, url_for
 from flask_cors import CORS
+from datetime import datetime
+from sqlalchemy import create_engine
+
+
 
 usuarios = [
     {"id": 1, "nome": "João Silva", "email": "joao@email.com", "senha": "123456", "tipo": "usuario"},
     {"id": 2, "nome": "Maria Suporte", "email": "maria@email.com", "senha": "abc123", "tipo": "suporte_n1"}
 ]
+
+chamados = []
 
 app = Flask(__name__)
 CORS(app)
@@ -39,6 +45,31 @@ def login():
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+@app.route('/chamado')
+def chamado():
+    return render_template('chamado.html')
+
+@app.route('/chamado', methods=['POST'])
+def abrir_chamado():
+    data = request.json
+    if not data:
+        return jsonify({"mensagem": "Dados inválidos!"}), 400
+    
+    chamado = {
+        "id": len(chamados) + 1,
+        "titulo": data.get("titulo"),
+        "descricao": data.get("descricao"),
+        "status": "aberto",
+        "usuario_id": data.get("usuario_id"),
+        "data-criacao": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+    chamados.append(chamado)
+    return jsonify({"mensagem": "Chamado criado com sucesso!", "chamado": chamado}), 201
+
+@app.route('/ver-chamado')
+def ver_chamado():
+    return render_template('Verchamado.html')
 
 
 
